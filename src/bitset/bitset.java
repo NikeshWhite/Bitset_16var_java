0651e1ac60ac6c04fb0bd32d5e52927115322c33
nikeshwhite <Nikesh.Social@yandex.ru>
2018-03-12 16:48:56 +0300

    Initial commit

---
+++ src/bitset/bitset.java
@@ -0,0 +1,31 @@
+package bitset;
+
+import java.util.*;
+
+class bitSet {
+
+    public int size;
+    public String name;`
+
+    public void addition (mnoj mnojestvo1, mnoj mnojestvo2) {
+       // mnojestvo3 = mnojestvo2 + mnojestvo1;
+    }
+
+    void umnojit(mnojestvo mnojestvo1, mnojestvo mnojestvo2) {
+
+    }
+
+    bitSet(String name, int size){
+
+        this.name = name;
+        this.size = size;
+
+    }
+
+}
+
+class mnoj {
+
+    ArrayList mnoj = new ArrayList();
+
+}
